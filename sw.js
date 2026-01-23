785df9f5170b854fd961a4de030143cdd8ab60ef
Ochir2023 <123377940+Ochir2023@users.noreply.github.com>
2026-01-23 22:42:00 +0300

    Refactor service worker cache implementation

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const CACHE_NAME='bjj-cache-v1';
-const urlsToCache=['./','./index.html','./manifest.json'];
-self.addEventListener('install', e=>{e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));});
-self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));});
+const CACHE='bjj-v1';
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
