5384527474cc4df7ff44e4e538ad6f8124e6de9a
Ochir2023 <123377940+Ochir2023@users.noreply.github.com>
2026-01-23 14:42:29 +0300

    Refactor service worker code formatting

--- sw.js
+++ sw.js
@@ -1,10 +1,4 @@
-const CACHE_NAME = 'bjj-cache-v1';
-const urlsToCache = ['./','./index.html','./manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+const CACHE_NAME='bjj-cache-v1';
+const urlsToCache=['./','./index.html','./manifest.json'];
+self.addEventListener('install', e=>{e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));});
+self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));});
