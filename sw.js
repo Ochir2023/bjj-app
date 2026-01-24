6848746a858c3ba6804f59e6acf8d8ab25e694af
Ochir2023 <123377940+Ochir2023@users.noreply.github.com>
2026-01-24 13:20:22 +0300

    Refactor cache handling code for readability

--- sw.js
+++ sw.js
@@ -3,22 +3,16 @@ const urlsToCache=['/','/index.html','/style.css','/app.js','/manifest.json'];
 
 self.addEventListener('install', e=>{
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('activate', e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
-    ))
-  );
+  e.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
+  )));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res=>res || fetch(e.request)));
 });
