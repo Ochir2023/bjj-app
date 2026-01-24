a3971de55caafc7f203a6db5961a36cdc8f6fea2
Ochir2023 <123377940+Ochir2023@users.noreply.github.com>
2026-01-24 14:22:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,6 @@
 const CACHE_NAME='bjj-cache-v1';
-const urlsToCache=['/','/index.html','/style.css','/app.js','/manifest.json'];
+const FILES_TO_CACHE=['index.html','app.js','manifest.json','style.css','icon-192.png','icon-512.png'];
 
-self.addEventListener('install', e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
-});
-
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(
-    keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
-  )));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(res=>res || fetch(e.request)));
-});
+self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES_TO_CACHE))); self.skipWaiting(); });
+self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
